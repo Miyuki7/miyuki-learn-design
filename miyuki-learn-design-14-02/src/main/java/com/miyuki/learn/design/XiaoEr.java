8dacf211170e17c32f52363667a45c23b058e83e
miyuki <yuzh7799@gmail.com>
2023-09-12 21:10:04 +0800

    命令模式完成

---
+++ miyuki-learn-design-14-02/src/main/java/com/miyuki/learn/design/XiaoEr.java
@@ -0,0 +1,32 @@
+package com.miyuki.learn.design;
+
+import com.miyuki.learn.design.cuisine.ICuisine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: miyuki
+ * @description: 重构后的小二
+ * @date: 2023/9/12 20:39
+ * @version: 1.0
+ */
+public class XiaoEr {
+
+    private Logger logger = LoggerFactory.getLogger(XiaoEr.class);
+
+    private List<ICuisine> cuisineList = new ArrayList<ICuisine>();
+
+    public void order(ICuisine cuisine) {
+        cuisineList.add(cuisine);
+    }
+
+    public synchronized void placeOrder() {
+        for (ICuisine cuisine : cuisineList) {
+            cuisine.cook();
+        }
+        cuisineList.clear();
+    }
+}
---
+++ miyuki-learn-design-14-02/src/main/java/com/miyuki/learn/design/cuisine/ICuisine.java
@@ -0,0 +1,12 @@
+package com.miyuki.learn.design.cuisine;
+
+/**
+ * @author: miyuki
+ * @description: 命令接口类
+ * @date: 2023/9/12 20:40
+ * @version: 1.0
+ */
+public interface ICuisine {
+
+    void cook();
+}
